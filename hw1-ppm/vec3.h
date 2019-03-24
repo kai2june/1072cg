1e1e201ba2a185dc0ac2610bb470510f4aacce08
kai2june <104703027@nccu.edu.tw>
2019-03-24 17:52:51 +0800

    learn_ray_tracing blue, purple mixed sphere done.

--- hw1-ppm/vec3.h
+++ hw1-ppm/vec3.h
@@ -32,7 +32,7 @@ class vec3 {
 		inline const vec3& operator+() const {return *this;}
 		inline vec3 operator-() const { return vec3(-e[0], -e[1], -e[2]);}
 		inline float operator[](int i) const { return e[i]; }//////
-		//inline float& operator[](int i) { return e[i]; }//////
+		inline float& operator[](int i) { return e[i]; }//////
 
 		inline vec3& operator+=(const vec3 &v2);
 		inline vec3& operator-=(const vec3 &v2);
@@ -81,6 +81,10 @@ inline vec3 operator*(float t, const vec3 &v){
 	return vec3(t*v.e[0], t*v.e[1], t*v.e[2]);
 }
 
+inline vec3 operator/(float t, const vec3 &v){
+	return vec3(t/v.e[0], t/v.e[1], t/v.e[2]);
+}
+
 inline vec3 operator/(vec3 v, float t){
 	return vec3(v.e[0]/t, v.e[1]/t, v.e[2]/t);
 }
@@ -88,6 +92,7 @@ inline vec3 operator/(vec3 v, float t){
 inline vec3 operator*(const vec3 &v, float t){
 	return vec3(t*v.e[0], t*v.e[1], t*v.e[2]);
 }
+
 
 inline float dot(const vec3 &v1, const vec3 &v2){
 	return v1.e[0] * v2.e[0] + v1.e[1] * v2.e[1] + v1.e[2] * v2.e[2];
