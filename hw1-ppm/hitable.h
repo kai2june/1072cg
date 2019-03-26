2c4a8ea2b9d8714e525aceb996842f228cc9cce1
kai2june <104703027@nccu.edu.tw>
2019-03-27 00:25:52 +0800

    diffuse material done

--- hw1-ppm/hitable.h
+++ hw1-ppm/hitable.h
@@ -5,10 +5,13 @@
 #include "vec3.h"
 #include "ray.h"
 
+class material;
+
 struct hit_record{
 	float t;
 	vec3 p;
 	vec3 normal;
+	material *mat_ptr;
 };
 
 class hitable{
--- hw1-ppm/main.cpp
+++ hw1-ppm/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <cfloat>
+#include <cmath>
 #include "vec3.h"
 #include "ray.h"
 #include "sphere.h"
@@ -44,14 +45,26 @@ vec3 color(const ray& r){
 	return (1.0-t)*vec3(1.0, 1.0, 1.0) + t*vec3(0.5, 0.7, 1.0);
 }*/
 
+vec3 random_in_unit_sphere(){
+	vec3 p;
+	do{
+		p = 2.0*vec3(drand48(), drand48(), drand48()) - vec3(1.0,1.0,1.0);
+	}while( p.squared_length() >= 1.0);
+	return p;	
+}
+
 vec3 color(const ray& r, hitable *world){
 	hit_record rec;
-	if( world -> hit(r, 0.0, FLT_MAX, rec) ){
+	if( world -> hit(r, 0.001, FLT_MAX, rec) ){
+		vec3 target = rec.p + rec.normal + random_in_unit_sphere();
+		return 0.5*color(ray(rec.p, target-rec.p), world);
+
 		vec3 N = rec.normal;
 		vec3 L = unit_vector( r.origin() - r.point_at_parameter(rec.t));
 		float cos = dot(N, L);
 		vec3 I = vec3(1,1,1);
 		return cos / I;
+
 //		return 0.5*vec3(rec.normal.x() +1.0, rec.normal.y() +1.0, rec.normal.z() +1.0);
 	}
 	vec3 unit_direction = unit_vector(r.direction());
@@ -75,10 +88,11 @@ int main(){
 	hitable* list[10];
 	list[0] = new sphere( vec3(0.0, -100.5, -1.0), 100);
 	list[1] = new sphere( vec3(0.0, 0.0, -1.0), 0.5);
-	//list[2] = new sphere( vec3(1.0, 0.0, -1.2), 0.5);
-	//list[3] = new sphere( vec3(-1.0, 0.0, -1.2), 0.5);
-	
-	hitable *world = new hitable_list(list, 2);
+	list[2] = new sphere( vec3(1.0, 0.0, -1.2), 0.5);
+	list[3] = new sphere( vec3(-1.0, 0.0, -1.2), 0.5);
+	list[4] = new sphere( vec3(-0.5, -0.4, -0.8), 0.2);
+
+	hitable *world = new hitable_list(list, 5);
 	camera cam;
 
 	for(int j = ny-1; j>=0; j--){
@@ -91,6 +105,7 @@ int main(){
 				col += color(r, world);	
 			}
 			col /= float(ns);
+			col = vec3(sqrt(col[0]), sqrt(col[1]), sqrt(col[2]));
 			int ir = int(255.99 * col[0]);
 			int ig = int(255.99 * col[1]);
 			int ib = int(255.99 * col[2]);
