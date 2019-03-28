5ad2f1ee53124afcf54af67b8614199ef5673d21
kai2june <104703027@nccu.edu.tw>
2019-03-28 19:38:43 +0800

    dielectric done

--- hw1-ppm/material.h
+++ hw1-ppm/material.h
@@ -6,6 +6,18 @@
 #include "vec3.h"
 #include "hitable.h"
 
+bool refract(const vec3& v, const vec3& n, float ni_over_nt, vec3& refracted) {
+	vec3 uv = unit_vector(v);
+	float dt = dot(uv, n);
+	float discriminant = 1.0 - ni_over_nt*ni_over_nt*(1-dt*dt);
+	if(discriminant > 0){
+		refracted = ni_over_nt*(uv - n*dt) - n*sqrt(discriminant);
+		return true;
+	}
+	else
+		return false;
+}
+
 vec3 reflect(const vec3& v, const vec3& n){
 	return v - 2.0*dot(v, n)*n;
 }
